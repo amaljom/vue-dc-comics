697ae66e7155d556d34193a1f04b39c4df5ab42f
amaljom <amaljom99@gmail.com>
2022-07-15 16:21:01 +0200

    inserimento del logo

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    
-  </div>
+<!-- CREO UN DIV DOVE METTERE L'HEADER -->
+<!-- ALL' INTERNO DI ESSO CREO DUE DIV CONTENTI RISPETTIVAMENTE LOGO E LA LISTA -->
+  <header>
+        <nav>
+            <!-- logo -->
+            <div>
+                <img src="../assets/dc-logo.png" alt="Non trovato">
+            </div>
+            <!-- lista -->
+            <div></div>
+        </nav>
+  </header>
 </template>
 
 <script>
